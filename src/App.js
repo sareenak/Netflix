afac9a6911a706b7c99325384b0484c33f17eeb5
sareenak <43492259+sareenak@users.noreply.github.com>
2022-02-05 04:46:52 +0400

    design completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "./Components/Header/NavBar";
 import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RawPost from "./Components/RawPost/RawPost";
 
 function App() {
   return(<div>
     <NavBar/>
+    <Banner/>
+    <RawPost/>
   </div>)
  
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Banner.css'
+
+function Banner() {
+  return (
+  <div className='banner'>
+      <div className='content'>
+          <h1 className='title'>Movie Name</h1>
+          <div className='banner_buttons'>
+              <button className='buttons'>Play</button>
+              <button className='buttons'>My List</button>
+
+          </div>
+          <div>
+          <h1 className='description'>
+          In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual 
+          </h1>
+          </div>
+          <div className='fade_bottom'>
+
+          </div>
+      </div>
+
+
+
+        </div>)
+}
+
+export default Banner;
