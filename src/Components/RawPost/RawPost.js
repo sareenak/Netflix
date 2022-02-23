5f72e9d0c6c3fd6f1388d6e6c6477f6358e00cae
sareenak <43492259+sareenak@users.noreply.github.com>
2022-02-23 14:40:41 +0400

    title and style dynamically render to rowpost

--- src/Components/RawPost/RawPost.js
+++ src/Components/RawPost/RawPost.js
@@ -21,7 +21,7 @@ function RawPost(props) {
       <h2 className=''>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj)=>
-        <img className='poster'  src={`${imageUrl+obj.backdrop_path}`} alt="Posters" />
+        <img className= {props.isSmall ?'smallPoster':'poster'}   src={`${imageUrl+obj.backdrop_path}`} alt="Posters" />
 
         )}
           
