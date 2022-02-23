2a490505ef676e269508cf98c28ec16808e5013e
sareenak <43492259+sareenak@users.noreply.github.com>
2022-02-23 22:53:30 +0400

    video played dynamically

--- src/Components/RawPost/RawPost.js
+++ src/Components/RawPost/RawPost.js
@@ -1,13 +1,14 @@
 import  React ,{useState,useEffect} from 'react';
 import './RawPost.css'
 import  YouTube from 'react-youtube' 
-import {imageUrl} from '../../constants/constants'
+import {api_Key, imageUrl} from '../../constants/constants'
 import axios from '../../axios';
 
 
 
 function RawPost(props) {
   const [movies,setMovies] = useState([])
+  const [urlId,setUrlId]=useState('')
   useEffect(() => {
     axios.get(props.url).then((response)=>{
       console.log(response.data)
@@ -21,19 +22,28 @@ function RawPost(props) {
     width: '100%',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
+      autoplay: 1,
     },
   };
+  const handleMovie=(id)=>{
+    axios.get(`/movie/${id}/videos?api_key=${api_Key}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('Trailer not available')
+      }
+    })
+  }
   return (<div className='row'>
       <h2 className=''>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj)=>
-        <img className= {props.isSmall ?'smallPoster':'poster'}   src={`${imageUrl+obj.backdrop_path}`} alt="Posters" />
+        <img onClick={()=>handleMovie(obj.id)} className= {props.isSmall ?'smallPoster':'poster'}   src={`${imageUrl+obj.backdrop_path}`} alt="Posters" />
 
         )}
           
          </div>
-         <YouTube videoId='2g811Eo7K8U' opts={opts} />
+       { urlId && <YouTube videoId={urlId.key} opts={opts} /> }
 
 
   </div>)
